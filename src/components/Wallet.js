fb3a6b9bcd52c0e6e3211f68565ace07f1715dd9
SnowyField <aswdqe1x@gmail.com>
2022-07-17 19:46:48 +0700

    thuan

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -2,45 +2,65 @@ import React from 'react'
 import { useState, useEffect } from 'react';
 
 import { formatEther } from 'ethers/lib/utils';
-import { useEthers, useEtherBalance, Rinkeby, Mainnet } from "@usedapp/core";
+import { useEthers, useEtherBalance, Mainnet, Kovan, Ropsten, Rinkeby, Goerli } from "@usedapp/core";
+
+import BalanceMenu from './BalanceMenu';
 
 
 function Wallet() {
     const { activateBrowserWallet, deactivate, account } = useEthers();
-    const rinkebyBalance = useEtherBalance(account, { chainId: Rinkeby.chainId });
     const mainnetBalance = useEtherBalance(account, { chainId: Mainnet.chainId });
+    const ropstenBalance = useEtherBalance(account, { chainId: Ropsten.chainId });
+    const kovanBalance = useEtherBalance(account, { chainId: Kovan.chainId });
+    const rinkebyBalance = useEtherBalance(account, { chainId: Rinkeby.chainId });
+    const goerliBalance = useEtherBalance(account, { chainId: Goerli.chainId });
 
-    const [address, setAddress] = useState(false);
+    const [address, setAddress] = useState(true);
+    const [balance, setBalance] = useState(null)
+
     const handleAddress = () => {
         return setAddress(!address);
     };
 
+
     return (
-        <div>
+        <div className='overflow-visible'>
             {account ?
-                <div>
-                    {address ? 
+                <div className='flex'>
+                    {address ?
                         <div className='flex'>
                             <h1 className='self-center text-xl'>Account address: {account.substring(0, 10) + '...' + account.substring(account.length - 4, account.length)}</h1>
                             <button onClick={() => handleAddress()} className='rounded-2xl border-2 px-2 py-1 ml-5 text-xl'>show</button>
                         </div>
-                    :
+                        :
                         <div className='flex'>
                             <h1 className='self-center text-xl'>Account address: {account}</h1>
                             <button onClick={() => handleAddress()} className='rounded-2xl border-2 px-2 py-1 ml-5 text-xl'>hide</button>
                         </div>
                     }
+                    <BalanceMenu setBalance={setBalance} />
 
-                    {rinkebyBalance && <h1>Balance: {rinkebyBalance}</h1>}
-                    
+                    {rinkebyBalance && kovanBalance ?
+                        
+                        ((balance == 'Mainnet') ? <h1>{balance + ' balance: ' + formatEther(mainnetBalance).substring(0, 5) + ' ETH'}</h1>
+                            : (balance == 'Ropsten') ? <h1>{balance + ' balance: ' + formatEther(ropstenBalance).substring(0, 5) + ' ETH'}</h1>
+                            : (balance == 'Kovan') ? <h1>{balance + ' balance: ' + formatEther(kovanBalance).substring(0, 5) + ' ETH'}</h1>
+                            : (balance == 'Rinkeby') ? <h1>{balance + ' balance: ' + formatEther(rinkebyBalance).substring(0, 5) + ' ETH'}</h1>
+                            : (balance == 'Goerli') ? <h1>{balance + ' balance: ' + formatEther(goerliBalance).substring(0, 5) + ' ETH'}</h1>
+                        : <h1>choose network</h1>)
+                        
+                    :
+                        <h1>Loading...</h1>}
+
                     <button onClick={deactivate}
                         className='rounded-2xl bg-red-600 hover:bg-red-800 px-4 py-3 m-2 text-white text-2xl font-semibold'>Disconnect</button>
                 </div>
-            :
+                :
                 <button onClick={activateBrowserWallet} className='rounded-2xl bg-indigo-600 hover:bg-indigo-800 px-4 py-3 m-2 text-white text-2xl font-semibold'> Connect </button>
             }
 
         </div>
+
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { DAppProvider, Mainnet, Rinkeby } from '@usedapp/core' 
+import { DAppProvider, Mainnet, Ropsten, Kovan, Rinkeby, Goerli } from '@usedapp/core' 
 import { getDefaultProvider } from 'ethers';
 
 const config = {
   readOnlyChainId: Rinkeby.chainId,
   readOnlyUrls: {
     [Mainnet.chainId]: getDefaultProvider('mainnet'),
-    [Rinkeby.chainId]: 'https://rinkeby.infura.io/v3/5b62b841c8c9436d8cce850e719a7014'
+    [Ropsten.chainId]: getDefaultProvider('ropsten'),
+    [Kovan.chainId]: getDefaultProvider('kovan'),
+    [Rinkeby.chainId]: 'https://rinkeby.infura.io/v3/5b62b841c8c9436d8cce850e719a7014',
+    [Goerli.chainId]: getDefaultProvider('goerli')
   },
   network: { Rinkeby },
   notifications: {
