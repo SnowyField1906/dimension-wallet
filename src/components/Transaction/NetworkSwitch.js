db845c34e20e089ec5ee3ebf76987aeaf77ce1b2
SnowyField <aswdqe1x@gmail.com>
2022-08-11 02:31:03 +0700

    thuan

--- src/components/Transaction/NetworkSwitch.js
+++ src/components/Transaction/NetworkSwitch.js
@@ -4,7 +4,7 @@ import { Mainnet, Kovan, Ropsten, Rinkeby, Goerli } from "@usedapp/core";
 function Network({ setNetworkId }) {
 
     return (
-        <div className="flex items-center justify-center" onClick={(e) => {setNetworkId(parseInt(e.target.value))}}>
+        <div className="flex items-center justify-center" onFocus={(e) => {setNetworkId(parseInt(e.target.value))}}>
             <div>
                 <input name="rating" type="radio" id="1" className="sr-only peer" value={Mainnet.chainId}/>
                 <label htmlFor="1" className="rounded-l inline-block px-6 py-2.5 border-2 bg-gray-600 text-white font-medium text-xs leading-tight uppercase peer-hover:bg-blue-600 peer-focus:outline-none peer-checked:bg-blue-700 transition duration-150 ease-in-out">
--- src/containers/Transaction.js
+++ src/containers/Transaction.js
@@ -1,39 +1,62 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Navigate } from 'react-router-dom';
 import { useEthers, useSendTransaction } from "@usedapp/core";
-
+import { isAddress } from "ethers/lib/utils"
 import NetworkSwitch from '../components/Transaction/NetworkSwitch';
 
 function Transaction() {
     const { account, switchNetwork } = useEthers()
     const { sendTransaction, state } = useSendTransaction()
 
-    var status = state.status
-
     const [address, setAddress] = useState()
     const [amount, setAmount] = useState()
 
+    const [networkError, setNetworkError] = useState('')
+    const [addressError, setAddressError] = useState('')
+    const [amountError, setAmountError] = useState('')
+
     const [networkId, setNetworkId] = useState()
 
-    const ref = useRef();
-    useEffect(() => {
-        ref.current = networkId;
-    }, [networkId]);
-
-    const send = async (amountWei) => {
+    const send = async(amountWei) => {
         await switchNetwork(networkId)
-        if (networkId !== ref.current) {
-            status = 'network has not changed'
-        }
+        // if (networkId !== ref.current) {
+        //     status = 'network has not changed'
+        // }
         sendTransaction({ to: address, value: amountWei.toString() })
     }
 
-    const amountHandle = e => {
-        setAmount(e.target.value)
-        if (e.target.value < 0 || e.target.value > 100) {
-            send(e.target.value)
+    const [status, setStatus] = useState(state.status)
+
+    useEffect(() => {
+        if (!networkId) {
+            setNetworkError('Invalid network!')
         }
-    }
+        else {
+            setNetworkError('')
+        }
+        if (!isAddress(address)) {
+            setAddressError('Invalid address!')
+        }
+        else {
+            setAddressError('')
+        }
+        if (0 > amount || amount > 100 || amount == null) {
+            setAmountError('Invalid amount!')
+        }
+        else {
+            setAmountError('')
+        }
+    }, [networkId, address, amount])
+
+    useEffect(() => {
+        if (!networkError && !addressError && !amountError) {
+            setStatus(state.status)
+        }
+        else {
+            setStatus(networkError + " " + addressError + " " + amountError)
+        }
+    }, [networkError, addressError, amountError])
+
 
     if (!account) {
         return (
@@ -48,13 +71,13 @@ function Transaction() {
 
                 <div className='flex'>
                     <p className='text-white text-2xl'>Address</p>
-                    <input type='text' value={address} onChange={e => setAddress(e.target.value)}
+                    <input type='text' value={address} onChange={(e) => setAddress(e.target.value)}
                         className='mx-5 form-control block w-full px-3 py-1.5 text-base font-normal text-white bg-gray-800 bg-clip-padding border-solid border-2 border-gray-500 rounded transition ease-in-out m-0 focus:text-white focus:bg-gray-800 focus:border-blue-500 focus:outline-none'
                     />
                 </div>
                 <div className='flex'>
                     <p className='text-white text-2xl'>Amount</p>
-                    <input type='number' value={amount} onChange={(e) => amountHandle(e)}
+                    <input type='number' value={amount} onChange={(e) => setAmount(e.target.value)}
                         className='mx-5 form-control block w-full px-3 py-1.5 text-base font-normal text-white bg-gray-800 bg-clip-padding border-solid border-2 border-gray-500 rounded transition ease-in-out m-0 focus:text-white focus:bg-gray-800 focus:border-blue-500 focus:outline-none'
                     />
                 </div>
